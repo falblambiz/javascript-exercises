5e7bb38879a0930e9d4dbbd6afa96d096b5b35b0
Felix <falblambiz@gmail.com>
2024-09-27 19:46:38 -0500

    Completes test 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    for (let x = string.length - 1; x >= 0; x--) {
+        reversedString = reversedString + string[x];
+    }
+    return reversedString
 };
 
 // Do not edit below this line

00a95443be546e151263451cf06f9ed7d83f7fba
Felix <falblambiz@gmail.com>
2024-09-27 19:36:53 -0500

    Completes test 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if (num < 0) { return 'ERROR'; }
+    
+    let repeatedString = "";
+    for (let x = 0; x < num; x++) {
+        repeatedString = repeatedString + string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
